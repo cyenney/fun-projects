14ee90b6edecca0c04134ec8174fb9ca63fab68c
cyenney <46497569+cyenney@users.noreply.github.com>
2020-07-27 19:48:24 -0400

    Add files via upload

---
+++ validateAnEmailAddress/validateAnEmailAddress/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace validateAnEmailAddress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // validate an email address
+            //So whats is considered an email address? IMO the string must contain "@" && ".com" for it to be valid.
+       
+            string userInput = "test@microsoft.com";
+
+            string @Valid = "@";
+            string comValid = ".com";
+
+            bool b = userInput.Contains(@Valid) && userInput.Contains(comValid);
+
+
+            if (b == true)
+            {
+                Console.WriteLine(userInput + " is a valid address");
+            }
+
+            else
+            {
+                Console.WriteLine(userInput + " is not a valid address");
+            }
+
+  
+            Console.ReadLine();
+
+   
+
+        }
+    }
+}
