3eae7c1e7fa195399808a19820fdc005e3a011dd
cyenney <46497569+cyenney@users.noreply.github.com>
2020-07-28 19:09:24 -0400

    Delete mfpp.py

--- mfpp.py
+++
@@ -1,9 +0,0 @@
-#My first python program
-
-num = int(input("Enter a number and I'll tell you if its even or odd"))
-
-
-if num % 2 == 0:
-    print(num, "is an even number")
-else:
-    print(num, "is an odd number")
