83a5a64c26f5ae7f3d1c3da4af040342ce258ed7
cyenney <46497569+cyenney@users.noreply.github.com>
2020-08-17 12:04:26 -0400

    Add files via upload

---
+++ fib/fib/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fib();
+        }
+
+        public static void fib()
+        {
+            Console.WriteLine("Enter a number to find the nth element in the fib sequence.");
+            int nth = Int32.Parse(Console.ReadLine());
+
+            int b = 0;
+            int i = 1;
+            int a = 0;
+
+            for (int j = 0; j < nth; j++)
+            {
+                a = b + i;
+                Console.WriteLine(" {0}", b);
+                b = i;
+                i = a;
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
